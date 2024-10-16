230b672586564aac3f49f1e2dcd8d245ad304669
Kwon <kdg5436@gmail.com>
2024-10-16 11:30:33 +0900

    권동균최고

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ def print_pyramid(n: int):
         for k in range(1, 2*i):
             print('*', end='')
         print()
-    raise NotImplementedError
 
 
 def print_inverted_pyramid(n: int):
     # TODO: implement this function
     for i in range(1,n):
         print(" "*(i) + "*"*(2*(n-i)-1))
-    raise NotImplementedError
 
 
 if __name__ == "__main__":
